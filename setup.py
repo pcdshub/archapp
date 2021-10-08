5c83d9ac67588b798ca5cad818215afa8a6d2506
Ken Lauer <klauer@slac.stanford.edu>
2021-10-08 14:35:27 -0700

    BLD/CI: update package to use versioneer, pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from setuptools import setup, find_packages
+import versioneer
+from setuptools import find_packages, setup
+
 import archapp
+
+with open("requirements.txt", "rt") as fp:
+    install_requires = [
+        line for line in fp.read().splitlines()
+        if line and not line.startswith("#")
+    ]
+
 
 setup(
     name='archapp',
-    version=archapp.__version__,
-    #packages=['archapp'],
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license="BSD",
+    include_package_data=True,
     packages=find_packages(),
     description='Archiver Appliance Python Interface',
     author='Zachary Lentz',
-    author_email='zlentz@slac.stanford.edu'
+    author_email='zlentz@slac.stanford.edu',
+    install_requires=install_requires,
+    python_requires=">=3.6",
 )
