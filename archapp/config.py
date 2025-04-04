4e7b6376d361c88919f9e575f475e608c46605f7
Kaushik Malapati <kaushikm@psbuild-rhel7-03.Slac.Stanford.EDU>
2025-04-03 22:59:05 -0700

    Updating hostname and increasing search_pvs timeout

--- archapp/config.py
+++ archapp/config.py
@@ -3,6 +3,6 @@ config.py defines default archive appliance server properties
 """
 import os
 
-hostname = os.environ.get("ARCHAPP_HOSTNAME", "pscaa02")
+hostname = os.environ.get("ARCHAPP_HOSTNAME", "pscaa01")
 data_port = int(os.environ.get("ARCHAPP_DATA_PORT", 17668))
 mgmt_port = int(os.environ.get("ARCHAPP_MGMT_PORT", 17665))
